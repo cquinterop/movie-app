8e031eb586da644538025595fcc01a0809c15165
cquinterop <cristianqpineda@gmail.com>
2025-03-08 19:25:49 +0100

    feat: add pre-commit githook with Husky, ESLint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: ['next/core-web-vitals', 'next/typescript'],
+		plugins: ['@typescript-eslint', 'prettier', 'react', 'react-hooks'],
+		rules: {
+			'prettier/prettier': 'error',
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'error',
+			'@typescript-eslint/sort-type-constituents': 'error',
+			"react-hooks/rules-of-hooks": "error",
+			"react-hooks/exhaustive-deps": "warn",
+			'react/jsx-sort-props': ['warn', { callbacksLast: true, multiline: 'first', shorthandFirst: true }],
+		},
+	}),
 ];
 
 export default eslintConfig;
